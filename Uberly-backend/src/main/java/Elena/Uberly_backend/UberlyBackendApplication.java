166a82ad239dbf8ebff83e6e04a2faf36baaf149
Elenaaaak <kekicelena@gmail.com>
2024-06-11 11:35:24 +0200

    dividing front and back end

---
+++ Uberly-backend/src/main/java/Elena/Uberly_backend/UberlyBackendApplication.java
@@ -0,0 +1,13 @@
+package Elena.Uberly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberlyBackendApplication.class, args);
+	}
+
+}
