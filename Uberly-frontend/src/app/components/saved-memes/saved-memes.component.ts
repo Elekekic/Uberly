fdba3c944f946e15cda78aa52818844555265a96
Elenaaaak <kekicelena@gmail.com>
2024-06-15 18:58:31 +0200

    added some components to be aware of how the routes i need

---
+++ Uberly-frontend/src/app/components/saved-memes/saved-memes.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saved-memes',
+  templateUrl: './saved-memes.component.html',
+  styleUrls: ['./saved-memes.component.scss']
+})
+export class SavedMemesComponent {
+
+}
