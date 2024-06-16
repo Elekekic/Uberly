e68853e7255aae65207896102c8cbc16643870d0
Elenaaaak <kekicelena@gmail.com>
2024-06-16 18:14:03 +0200

    starting the layout with the mobile firstly

--- Uberly-frontend/src/app/components/navbar/navbar.component.ts
+++ Uberly-frontend/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements AfterViewInit {
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit() {
+    const primaryNav = this.renderer.selectRootElement('.primary-navigation', true);
+    const navToggle = this.renderer.selectRootElement('.mobile-nav-toggle', true);
+
+    this.renderer.listen(navToggle, 'click', () => {
+      const visibility = primaryNav.getAttribute('data-visible');
+
+      if (visibility === "false") {
+        primaryNav.setAttribute('data-visible', 'true');
+        navToggle.setAttribute('aria-expanded', true);
+      } else {
+        primaryNav.setAttribute('data-visible', 'false');
+        navToggle.setAttribute('aria-expanded', false);
+      }
+    });
+  }
 }
